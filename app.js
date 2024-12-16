3f2df5085f0db4c86c5fd6f792b2074981570ae6
Amartya Anand <dev.amartya@outlook.com>
2024-12-16 22:36:15 +0530

    Sending response

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const http = require("http");
 //Server created
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  process.exit(); //Stops event loop
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>MERN</title></head>");
+  res.write("<body><h1>Mongo / Express / React / Node</h1></body>");
+  res.write("</html>");
+  res.end();
+  // process.exit(); //Stops event loop
 });
 
 const PORT = 3000;
