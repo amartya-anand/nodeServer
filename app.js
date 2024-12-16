3d245c9008e89442fea2c3dec8787a45d631b9bd
Amartya Anand <dev.amartya@outlook.com>
2024-12-16 22:52:24 +0530

    Routing requests

--- app.js
+++ app.js
@@ -2,7 +2,22 @@ const http = require("http");
 
 //Server created
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  // console.log(req.url, req.method, req.headers);
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>MERN</title></head>");
+    res.write("<body><h1>Mongo / Express / React / Node</h1></body>");
+    res.write("</html>");
+    return res.end();
+  } else if (req.url === "/products") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>DevOps</title></head>");
+    res.write("<body><h1>Node</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>MERN</title></head>");
